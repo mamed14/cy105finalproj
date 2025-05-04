554f5cdf42c2dacbd28a5edfd660affd31354536
mamed14 <maya.isabella.medrano@gmail.com>
2025-05-04 16:34:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -71,6 +71,6 @@ if model:
         similar = df.iloc[indices[0]]["Name"].tolist()
 
         st.markdown(f"### ➥ Predicted Score: **{round(avg_score, 2)} / 5** ")
-        st.markdown("#### Similar Movies You've Rated:")
-        st.write("⋆", similar[0])
-        st.write("⋆", similar[1])
+        st.markdown("####      Similar Movies You've Rated:")
+        st.write("     ⋆", similar[0])
+        st.write("     ⋆", similar[1])
