0dcea1dedf939b9261cce750d4f7cb1dbaddcde7
mamed14 <maya.isabella.medrano@gmail.com>
2025-05-04 16:31:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ if model:
 
         st.markdown(f"### 🎯 Predicted Score: **{round(avg_score, 2)} / 5**")
         st.markdown("#### Similar Movies You've Rated:")
-        st.write("★", similar[0])
-        st.write("★", similar[1])
+        st.write("⋆", similar[0])
+        st.write("⋆", similar[1])
